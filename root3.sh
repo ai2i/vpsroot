820f8e454e6d01674adb3e9f69193f7b55351434
甬哥kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-02-22 12:53:33 +0800

    Update and rename root2.sh to root3.sh

--- root3.sh
+++ root3.sh
@@ -24,12 +24,16 @@ prl=`grep PermitRootLogin /etc/ssh/sshd_config`
 pa=`grep PasswordAuthentication /etc/ssh/sshd_config`
 if [[ -n $prl && -n $pa ]]; then
 readp "自定义root密码:" mima
+if [[ -n $mima ]]; then
 echo root:$mima | $su chpasswd root
 $su sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 $su sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 $su service sshd restart
 green "VPS当前用户名：root"
 green "vps当前root密码：$mima"
+else
+red "未输入相关字符，启用root账户或密码更改失败" && exit 1
+fi
 else
 red "当前vps不支持root账户或无法自定义root密码" && exit 1
 fi
